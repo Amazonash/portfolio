4397c28d001a9c7df91742184afaffd0a77d0e59
Amazonash <66981801+Amazonash@users.noreply.github.com>
2021-08-18 19:28:50 -0400

    fix header

--- src/Header.js
+++ src/Header.js
@@ -5,9 +5,9 @@ class Header extends Component{
         return (
         <nav>
   <div class="nav-wrapper">
-    <a href="/home" class="brand-logo">Ashley's Portfolio</a>
+    <a href="https://amazonash.github.io/portfolio/#/home" class="brand-logo">Ashley's Portfolio</a>
     <ul id="nav-mobile" class="right hide-on-med-and-down">
-      <li><a href="/portfolio">Portfolio</a></li>
+      <li><a href="https://amazonash.github.io/portfolio/#/portfolio">Portfolio</a></li>
     </ul>
   </div>
 </nav>
